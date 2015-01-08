f8ce5e004049ea5cb16a2e4499bef4e23048ce50
mitch-b <mitch.barry@gmail.com>
2015-01-08 13:05:12 -0600

    try adding port number customizations

---
+++ js/localhost_link_helper.js
@@ -0,0 +1,34 @@
+var localhostLinkHelper = (function($){
+    /// Replace the placeholder text your-port-number with the name stored in sessionStorage or create a text input if it is not in sessionStorage
+    var portNumber = window.sessionStorage.getItem('portNumber');
+    var links = $("a[href*='your-port-number']");
+    if (portNumber) {
+        // update all linksa
+        links.replaceWith(function(){
+            return '<a href="' + this.href.replace(/your-port-number/, portNumber) + '">' + this.text.replace(/your-port-number/, portNumber) + ' <a href="#" class="clear-portNumber">(<i class="fa fa-times"></i> clear port number)</a>';
+        });
+        $('.clear-portNumber').click(function(){
+            window.sessionStorage.removeItem('portNumber');
+            window.location = window.location;
+        });
+    } else {
+        // update all links with localhostLinkHelper input
+        for (var i = 0; i < links.length; ++i){
+            (function(i){
+            var div = document.createElement('div');
+            div.id = 'portNumber-' + i;
+            div.style.display='inline-block';
+            var linkStart = links[i].text.substr(0, links[i].text.indexOf('your-port-number'));
+            var linkEnd = links[i].text.substr(links[i].text.indexOf('your-port-number') + 'your-port-number'.length);
+            div.innerHTML = linkStart + '<input type="text" placeholder="your-port-number"></input>' + linkEnd + ' <button>set</button>';
+            links[i].parentElement.replaceChild(div, links[i]);
+            $('button', div).click(function(){
+                var portNumber = $('input', div).val();
+                window.sessionStorage.setItem('portNumber', portNumber);
+                window.open(links[i].href.replace(/your-port-number/, portNumber));
+                window.location = window.location;
+            });
+            }(i));
+        }
+    }
+}(jQuery));
--- modules/web api/_posts/2000-01-04-checking-our-results.markdown
+++ modules/web api/_posts/2000-01-04-checking-our-results.markdown
@@ -8,24 +8,24 @@ published: true
 1. Click Start in the toolbar to run your Web Application.
 2. You will get a forbidden message, because right now we have no root resource for IIS to display. Let's navigate to our Todos resource entity set using Google Chrome or Mozilla Firefox.
 
-> IE (if JSON result) will automatically ask you to download the response. Just makes it slightly more inconvenient.
+> Internet Explorer (if JSON result) will automatically ask you to download the response. Just makes it slightly more inconvenient.
 
-    http://localhost:12345/odata/Todos
+    http://localhost:your-port-number/odata/Todos
 
 3. (Optional) Alternatively, you can use an extension like Postman for Chrome (recommended), or REST client for Firefox.
 
 Try several OData calls:
 
-    http://localhost:12345/odata/Todos(1)
+    http://localhost:your-port-number/odata/Todos(1)
 
-    http://localhost:12345/odata/Todos(1)/UserCompletedBy
+    http://localhost:your-port-number/odata/Todos(1)/UserCompletedBy
 
-    http://localhost:12345/odata/Todos?$filter= Completed eq false
+    http://localhost:your-port-number/odata/Todos?$filter= Completed eq false
 
-    http://localhost:12345/odata/Todos?$filter= Finished eq false
-    http://localhost:12345/odata/Todos?$filter= Finished eq true
+    http://localhost:your-port-number/odata/Todos?$filter= Finished eq false
+    http://localhost:your-port-number/odata/Todos?$filter= Finished eq true
 
-    http://localhost:12345/odata/Todos/$count
-    http://localhost:12345/odata/Todos?$format=json
+    http://localhost:your-port-number/odata/Todos/$count
+    http://localhost:your-port-number/odata/Todos?$format=json
 
 You'll notice `$count` and `$format` are not supported. We'll check on those in the next section.
